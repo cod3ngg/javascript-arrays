2ba67d1a7aece1e378cc5e6df375a542ec4f6853
cod3ngg <150222307+cod3ngg@users.noreply.github.com>
2024-08-29 16:18:18 +0800

    Passed sumAll, leapYears, and tempConversion.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+    let numAll = 0;
+    if (!Number.isInteger(min) || !Number.isInteger(max)) {
+        return `ERROR`;
+    }
+    if (min > max) {
+        const minNew = min;
+        min = max;
+        max = minNew;
+    }
+    if (min < 0 || max < 0) {
+        return `ERROR`;
+    }
 
+    for (let i = min; i <= max; i++){
+        numAll += i;
+    }
+
+    return numAll;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let convertedTemp = (temp - 32) / 1.8;
+  let roundNum = Math.pow(10, 1);
+  convertedTemp = Math.round(convertedTemp * roundNum) / roundNum;
+  return convertedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let convertedTemp = temp * 1.8 + 32;
+  let roundNum = Math.pow(10, 1);
+  convertedTemp = Math.round(convertedTemp * roundNum) / roundNum;
+  return convertedTemp;
 };
 
 // Do not edit below this line
